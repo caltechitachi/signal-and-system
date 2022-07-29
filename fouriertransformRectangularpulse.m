aa438fd6b61c2305fae7f107ee4831e0b64dee04
caltechitachi <88483399+caltechitachi@users.noreply.github.com>
2022-07-29 08:54:15 +0700

    Update fouriertransformRectangularpulse.m

--- fouriertransformRectangularpulse.m
+++ fouriertransformRectangularpulse.m
@@ -1,4 +1,4 @@
-t= sym('t')
+t= sym('t')  %initalize variable without attach number, this only function to create a formula
 f = rectangularPulse(-1/2,1/2,t);
 f_FT = fourier(f)
 
@@ -8,6 +8,6 @@ trituyetdoi_f_FT = sqrt(real((f_FT)^2) + imag((f_FT)^2))
 
 ft = atan((imag(f_FT))/(real(f_FT)))
 
-%v? riêng ?? th? nhé 
+
 
 
