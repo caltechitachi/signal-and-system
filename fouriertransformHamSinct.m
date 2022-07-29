e49bac62fe8e231407fb7c163093aaa7a15b1e9b
caltechitachi <88483399+caltechitachi@users.noreply.github.com>
2022-07-29 08:57:56 +0700

    Update fouriertransformHamSinct.m

--- fouriertransformHamSinct.m
+++ fouriertransformHamSinct.m
@@ -7,9 +7,9 @@ f_FT = fourier(f)
 
 
 %pho pha
-ft = atan((imag(f_FT))/(real(f_FT)))
+ft = atan((imag(f_FT))/(real(f_FT))) %tinh pho pha dung cong thuc
 
 %ve pho
 w = -2*pi: 0.8: 2*pi;
 gt = (2*(pi.*heaviside(pi - w) - pi.*heaviside(- w - pi)))/pi
-stem(w,gt)
+stem(w,gt)  %tin hieu roi rac su dung stem
